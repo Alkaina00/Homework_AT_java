2c0bb261b4133bdb820e34cbf9df7a96697e21d3
Alina Gainulina <alina.gajnullina2015@yandex.ru>
2026-04-20 13:42:36 +0500

    Поправила дз2: Добавлен п. 4 Поправлено сохранение п.3 Добавлено форматирование до двух после запятой для денег

--- src/main/java/org/example/homework2/BusinessCoach.java
+++ src/main/java/org/example/homework2/BusinessCoach.java
@@ -5,8 +5,9 @@ import java.util.Random;
 public class BusinessCoach {
     public static void main(String[] args) {
         Random random = new Random();
-        int age = random.nextInt(80);
+        int age = random.nextInt(100);
         double moneyAccount = random.nextDouble(100_000);
+        moneyAccount = Math.round(moneyAccount * 100.0) / 100.0;
 
         System.out.println("Возраст: " + age + "\n" + "Сумма на счете: " + moneyAccount);
 
@@ -14,15 +15,18 @@ public class BusinessCoach {
         boolean blacklist = false;
 
         boolean fullAge = age >= 18;
-        boolean lotOfMoney = moneyAccount > 50_000;
+        boolean invitationOrLotOfMoney = invitation || moneyAccount > 50_000;
+        boolean notBlacklist = !blacklist;
 
-        boolean result = fullAge && (invitation || lotOfMoney) && !blacklist;
+        boolean result = fullAge && invitationOrLotOfMoney && !blacklist;
 
-        System.out.println("Есть приглашение? " + invitation);
-        System.out.println("Есть в черном списке? " + blacklist);
         System.out.println("Есть 18 лет? " + fullAge);
-        System.out.println("На счете больше 50_000? " + lotOfMoney);
+        System.out.println("Есть приглашение или на счёте больше 50000? " + invitationOrLotOfMoney);
+        System.out.println("Посетитель не в черном списке? " + notBlacklist);
 
         System.out.println("\n" + "Пользователь проходит? " + result);
+
+        double fee = moneyAccount * 0.075;
+        System.out.println("Обязательный добровольный взнос: " + String.format("%.2f", fee));
     }
 }
